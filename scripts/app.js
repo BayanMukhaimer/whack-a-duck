399496a397e476a89c1735570377363f95e50485
Bayan <bayanessam40@gmail.com>
2025-07-09 13:56:53 +0300

    add handle click and increment score

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,14 @@
 function init() {
     //All code goes here
     const gridElem = document.querySelector('.grid')
+    const scoreElem = document.querySelector('#score-display')
 
     const cells = []
     const gridWidth = 10
     const numberOfCells = gridWidth * gridWidth
 
-    let duckPosition = 55
+    let duckPosition = 3
+    let score = 0
 
     function addDuck() {
         cells[duckPosition].classList.add('duck')
@@ -25,6 +27,15 @@ function init() {
         
     }
 
+    function handleClick(event) {
+        console.log('Handle Click has run!')
+        if (event.target.classList.contains('duck')) {
+            score += 10
+            scoreElem.textContent = `Your score is ${score}`
+            console.log(score)
+        }
+    }
+
     function createGrid() {
         // for every cell we require creat a div
         // append this cell to our grid
@@ -32,6 +43,7 @@ function init() {
             const cell = document.createElement('div')
             // cell.classList.add('duck')
             cell.textContent = i
+            cell.addEventListener('click', handleClick)
             cells.push(cell)
             gridElem.appendChild(cell)
         }
