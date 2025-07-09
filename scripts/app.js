847f74644bef0edc0cabfc10b09be04669875b80
Bayan <bayanessam40@gmail.com>
2025-07-09 11:15:12 +0300

    creat grid

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+function init() {
+    //All code goes here
+    const gridElem = document.querySelector('.grid')
+
+    function createGrid() {
+        // for every cell we require creat a div
+        // append this cell to our grid
+        for (let i =0; i < 100; i++) {
+            const cell = document.createElement('div')
+            cell.textContent = i
+            gridElem.appendChild(cell)
+        }
+    }
+    createGrid()
+}
+
+document.addEventListener('DOMContentLoaded', init)
